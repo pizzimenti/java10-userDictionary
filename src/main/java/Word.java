5bb91e1fe98045d2b41c15db9201b59c408f4500
Brad Pizzimenti <brad@gennakersystems.com>
2016-02-19 16:17:17 -0800

    add ArrayList for holding multiple words

--- src/main/java/Word.java
+++ src/main/java/Word.java
@@ -1,11 +1,20 @@
+import java.util.ArrayList;
+
 public class Word {
   private String mWord;
+  private static ArrayList<Word> wordList = new ArrayList<Word>();
 
   public Word(String word) {
     mWord = word;
+    wordList.add(this);
   }
 
   public String getWord() {
     return mWord;
   }
+
+  public static ArrayList<Word> all() {
+    return wordList;
+  }
+
 }
--- src/test/java/WordTest.java
+++ src/test/java/WordTest.java
@@ -15,6 +15,14 @@ public class WordTest {
     assertEquals("sauna", testWord.getWord());
   }
 
+  @Test
+  public void all_returnsWordList_twoWords() {
+    Word testWord1 = new Word("sauna");
+    Word testWord2 = new Word("sweatlodge");
+    assertTrue(Word.all().contains(testWord1));
+    assertTrue(Word.all().contains(testWord2));
+  }
+
   //an ancient Finnish word referring to the traditional Finnish bath and to the bathhouse itself
 
-} //end of WordTest
+}
